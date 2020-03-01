a6ea115edf648b64aa9d1bc72654fc3519e7752e
Ben S <bendev@Bens-MacBook-Air.local>
2020-02-29 22:32:09 -0500

    pic upload

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :ideas
-  root 'home#show'
+  root to: redirect('/ideas')
 end
